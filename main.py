8520ece690d9929737c50b89d28296cd482e0583
PetercSnk <peter.ch.snook@gmail.com>
2022-09-15 23:08:59 +0100

    change waveforms to use fourier series

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from random import sample
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -13,8 +14,8 @@ def load_audio():
     plt.show()
 
 sample_rate = 44100
-frequency = 5
-length = 2.0
+frequency = 2
+length = 1
 amplitude = 1
 phase = 0
 period = length/frequency
@@ -23,12 +24,45 @@ sine = amplitude*np.sin(2*np.pi*frequency*x+phase)
 sawtooth = -(2*amplitude/np.pi)*np.arctan(1/(np.tan(x*np.pi/period)))
 reverse_sawtooth = (2*amplitude/np.pi)*np.arctan(1/(np.tan(x*np.pi/period)))
 triangle = (4*amplitude/period)*np.abs(((x-period/4+phase)%period)-period/2)-amplitude
+square = ""
 
 
-plt.plot(x, sine)
+plt.plot(x, sawtooth)
 plt.show()
 
 
 #sine_wave *= 32767
 #sine_wave = np.int16(sine_wave)
 #wavfile.write("file.wav", sample_rate, sine_wave)
+
+class Wave:
+    def __init__(self, waveform, f, l, a = 1, p = 0, sampleRate = 44100):
+        self.f = f
+        self.l = l
+        self.a = a
+        self.p = p
+        self.sampleRate = sampleRate
+        self.x = np.linspace(0.000001, l, sampleRate)
+        self.y = self.createY(waveform)
+
+    def createY(self, waveform):
+        period = self.l/self.f
+        if waveform == "sine":
+            return self.a*np.sin(2*np.pi*self.f*self.x+self.p)
+        elif waveform == "sawtooth":
+            return -(2*self.a/np.pi)*np.arctan(1/(np.tan(self.x*np.pi/period)))
+        elif waveform == "reverseSawtooth":
+            return (2*self.a/np.pi)*np.arctan(1/(np.tan(self.x*np.pi/period)))
+        elif waveform == "triangle":
+            return (4*self.a/period)*np.abs(((self.x-period/4+self.p)%period)-period/2)-self.a
+        else:
+            raise TypeError("invalid waveform")
+    
+    def plot(self):
+        plt.plot(self.x, self.y)
+        plt.show()
+
+
+#if __name__ == "__main__":
+#    wave = Wave("sine", 1, 1.0)
+#    wave.plot()
