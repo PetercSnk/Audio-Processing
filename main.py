d62d4586943ed211e06d9caba2cb9f8ad2de97ab
PetercSnk <peter.ch.snook@gmail.com>
2022-09-16 21:00:38 +0100

    check what increases amp with fourier series

--- main.py
+++ main.py
@@ -14,21 +14,52 @@ def load_audio():
     plt.show()
 
 sample_rate = 44100
-frequency = 2
+frequency = 440
 length = 1
 amplitude = 1
 phase = 0
 period = length/frequency
-x = np.linspace(0.000001, length, sample_rate)
+x = np.linspace(0.000001, length, sample_rate*length)
 sine = amplitude*np.sin(2*np.pi*frequency*x+phase)
 sawtooth = -(2*amplitude/np.pi)*np.arctan(1/(np.tan(x*np.pi/period)))
 reverse_sawtooth = (2*amplitude/np.pi)*np.arctan(1/(np.tan(x*np.pi/period)))
 triangle = (4*amplitude/period)*np.abs(((x-period/4+phase)%period)-period/2)-amplitude
 square = ""
 
+#plt.plot(x, sawtooth)
+#plt.show()
 
-plt.plot(x, sawtooth)
+x1= np.linspace(0.000001, 1, 44100)
+s1 = 1*np.sin(2*np.pi*2*x1)
+s2 = 1/2*np.sin(2*np.pi*4*x1)
+s3 = 1/3*np.sin(2*np.pi*6*x1)
+s4 = 1/4*np.sin(2*np.pi*8*x1)
+s5 = 1/5*np.sin(2*np.pi*10*x1)
+s6 = 1/6*np.sin(2*np.pi*12*x1)
+s7 = 1/7*np.sin(2*np.pi*14*x1)
+
+s4 = s1+s2+s3+s4+s5+s6+s7
+
+#plt.plot(x1, s4)
+#plt.show()
+
+y = np.zeros(sample_rate*length)
+for k in range(1,51):
+    y += amplitude*np.sin(2*np.pi*frequency*k*x)/k
+
+plt.plot(x, y)
 plt.show()
+
+
+
+
+
+
+
+
+
+
+
 
 
 #sine_wave *= 32767
