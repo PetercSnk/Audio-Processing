9725650bf483a54477794e07b94e5584cb100984
petercsnk <peter.ch.snook@gmail.com>
2022-12-05 14:03:16 +0000

    check left and middle, add right

--- main.py
+++ main.py
@@ -35,29 +35,30 @@ def createY(waveform, freq, duration, amp = 1, phase = 0, sampleRate = 44100, ha
 
 # add short wave to long wave with splicing [::] select start 
 def add(wave1, wave2, offsetFreq1 = 0, offsetFreq2 = 0):
-    # lenghts of waves are total number of samples
-    wave1len = wave1.duration*wave1.sampleRate
-    wave2len = wave2.duration*wave2.sampleRate
-    # generated wave will be the length of the largest wave + its offset - other waves offset
-    wave3len = max([wave1len+offsetFreq1-offsetFreq2, wave2len+offsetFreq2-offsetFreq1])
-    #print(wave1len+offsetFreq1-offsetFreq2, wave2len+offsetFreq2-offsetFreq1)
+    # lengths of waves are total number of samples
+    wave1Length = wave1.duration*wave1.sampleRate
+    wave2Length = wave2.duration*wave2.sampleRate
+    minOffset = min([offsetFreq1, offsetFreq2])
+    maxOffset = max([offsetFreq1, offsetFreq2])
+    maxLength = max([wave1Length+offsetFreq1, wave2Length+offsetFreq2])
+    minLength = min([wave1Length+offsetFreq1, wave2Length+offsetFreq2])
+    wave3Length = maxLength-minOffset
     # create array of zeros of wave3len
-    np.zeros(wave3len)
-    midStop = min([wave1len+offsetFreq1, wave2len+offsetFreq2])
-    if  offsetFreq1 > wave2len+offsetFreq2 or offsetFreq2 > wave1len+offsetFreq1:
-        # ?? 
+    np.zeros(wave3Length)
 
     if offsetFreq1 < offsetFreq2:
-        left = wave1.y[0:offsetFreq2-offsetFreq1]
-        middle1 = wave1.y[offsetFreq2:midStop-offsetFreq1]
-        middle2 = wave2.y[0:midStop-offsetFreq2]
+        offsetDifference = offsetFreq2-offsetFreq1
+        left = wave1.y[0:offsetDifference]
+        middle1 = wave1.y[offsetDifference:minLength-offsetFreq1]
+        middle2 = wave2.y[0:minLength-offsetFreq2]
 
-    elif offsetFreq1 > offsetFreq2:
-        left = wave2.y[0:offsetFreq1-offsetFreq2]
-        middle1 = wave1.y[0:midStop-offsetFreq1]
-        middle2 = wave2.y[offsetFreq1:midStop-offsetFreq2]
+    elif offsetFreq2 < offsetFreq1:
+        offsetDifference = offsetFreq1-offsetFreq2
+        left = wave2.y[0:offsetDifference]
+        middle1 = wave1.y[0:minLength-offsetFreq1]
+        middle2 = wave2.y[offsetDifference:minLength-offsetFreq2]
     else:
-        left = None
+        left = False
     
 
 
